fd9cccf6c12902be8888e1728826d7ca7dc94965
Carlos Henrique <fariascarlos2346@gmail.com>
2025-10-28 08:55:37 -0300

    pegando os valores

--- api.js
+++ api.js
@@ -1,1 +1,16 @@
+const MOEDAS = ['bitcoin', 'ethereum', 'cardano', 'dogecoin', 'litecoin']
+
 const API_URL = 'https://api.coingecko.com/api/v3/simple/price'
+
+async function buscarPrecos() {
+
+    const ids = MOEDAS.join(',')
+    const url = `${API_URL}?ids=${ids}&vs_currencies=brl`
+
+    const response = await fetch(url)
+    const dados = await response.json()
+
+    console.log(dados)
+}
+
+buscarPrecos()
